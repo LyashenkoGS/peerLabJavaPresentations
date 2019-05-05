f1a2d9c4da7014c688609c3ab9140298374a70f3
Hryhorii Liashenko <ext-hryhorii.liashenko@here.com>
2019-05-05 14:48:32 +0300

    crypto mvp

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cryptography for Java developers samples
+
+## Description - https://docs.google.com/document/d/1vFIdULBFTUo3dp3wuoJEsXUL7I19ZG5TW9y8VmdTQtk/edit?usp=sharing
--- src/test/java/cryptodemo/AppTest.java
+++ src/test/java/cryptodemo/AppTest.java
@@ -1,20 +1,67 @@
 package cryptodemo;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
-     * Rigorous Test :-)
+     * Stream cypher
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void sendHTTPS_GET() throws IOException, NoSuchAlgorithmException, KeyManagementException {
+        //DIsable SSL certificate validation
+        SSLContext context = SSLContext.getInstance("TLSv1.2");
+        TrustManager[] trustManager = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certificate, String str) {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certificate, String str) {
+                    }
+                }
+        };
+        context.init(null, trustManager, new SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+        //Set proxy
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "8080");
+        System.setProperty("com.sun.net.ssl.checkRevocation", "false");
+        //Send a GET request
+        String url = "https://www.google.com/";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        //print result
+        System.out.println(response.toString());
     }
 }
